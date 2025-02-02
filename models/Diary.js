3313d249513f29454efc8a7eae8ff2f78acff224
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-02-02 07:34:01 +0200

    Create Diary.js

---
+++ models/Diary.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Diary Schema
+const diarySchema = new mongoose.Schema({
+    userId: { type: String, required: true }, // User's WhatsApp ID
+    note: { type: String, required: true },   // Diary note
+    timestamp: { type: Date, default: Date.now } // Timestamp of the note
+});
+
+// Diary Model
+const Diary = mongoose.model('Diary', diarySchema);
+
+module.exports = Diary;
