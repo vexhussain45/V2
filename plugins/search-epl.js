44f53f7d0981c3fb45ce961ed37d41e7af81600e
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-02-05 09:33:27 +0200

    Update search-epl.js

--- plugins/search-epl.js
+++ plugins/search-epl.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+/*const axios = require('axios');
 const config = require('../config');
 const { cmd } = require('../command');
 const { fetchJson } = require('../lib/functions');
@@ -35,3 +35,46 @@ cmd({
     reply('Something went wrong. Unable to fetch EPL standings.');
   }
 });
+
+ */
+const axios = require('axios');
+const config = require('../config');
+const { cmd } = require('../command');
+const { fetchJson } = require('../lib/functions');
+
+cmd({
+  pattern: 'epl',
+  react: '⚽',
+  desc: 'Display current EPL standings',
+  category: 'sports',
+  filename: __filename
+}, async (conn, mek, m, { from, reply }) => {
+  try {
+    // Retrieve the data from the API
+    const data = await fetchJson('https://api.dreaded.site/api/standings/PL');
+    
+    // Access the standings from the "data" property
+    const standings = data.data;
+    
+    // Initialize the message with the header
+    let message = '🏆 *EPL TABLE STANDINGS*\n\n';
+    
+    // Check if standings is an array
+    if (Array.isArray(standings)) {
+      standings.forEach(team => {
+        message += `${team.position}. ${team.team} - ${team.points} points\n`;
+      });
+    } else {
+      // If standings is not an array, handle accordingly
+      message += 'Standings data is not available.';
+    }
+    
+    // Send the message as plain text
+    await conn.sendMessage(from, { text: message }, { quoted: mek });
+  } catch (error) {
+    console.error('Error fetching EPL standings:', error);
+    reply('Something went wrong. Unable to fetch EPL standings.');
+  }
+});
+
+
