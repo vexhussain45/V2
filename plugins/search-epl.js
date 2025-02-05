104393f8395db8f6c4e850d5911c6be2666613b5
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-02-05 12:46:05 +0200

    Update search-epl.js

--- plugins/search-epl.js
+++ plugins/search-epl.js
@@ -1,10 +1,11 @@
-/*const axios = require('axios');
+const axios = require('axios');
 const config = require('../config');
 const { cmd } = require('../command');
 const { fetchJson } = require('../lib/functions');
 
 cmd({
   pattern: 'epl',
+  alias: 'englishspremierleague',
   react: '⚽',
   desc: 'Display current EPL standings',
   category: 'sports',
@@ -27,15 +28,39 @@ cmd({
       // In case the structure is different, display the raw data
       message += JSON.stringify(standings, null, 2);
     }
-    
+        
+        // Send the standings with an image
+    await conn.sendMessage(from, {
+      image: { url: `https://i.ibb.co/4g5ZZnWZ/mrfrankofc.jpg` }, // Image URL
+      caption: message,
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363304325601080@newsletter',
+          newsletterName: '『 𝐒𝐔𝐁𝐙𝐄𝐑𝐎 𝐌𝐃 』',
+          serverMessageId: 143
+        }
+      }
+    }, { quoted: mek });
+
+  } catch (error) {
+    console.error('Error fetching Premier League standings:', error);
+    reply('Something went wrong. Unable to fetch Premier League standings.');
+  }
+});
+    /*
     // Send the message back to the chat
     await conn.sendMessage(from, { text: message }, { quoted: mek });
   } catch (error) {
     console.error('Error fetching EPL standings:', error);
     reply('Something went wrong. Unable to fetch EPL standings.');
   }
-});
-*/
+});*/
+
+
+/*
 const axios = require('axios');
 const config = require('../config');
 const { cmd, commands } = require('../command');
@@ -103,3 +128,4 @@ cmd({
     reply('Something went wrong. Unable to fetch Premier League standings.');
   }
 });
+*/
