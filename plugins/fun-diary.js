e82ece71f662d54064229126af8bd8f4f59b5776
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-03-06 17:14:56 +0200

    Update fun-diary.js

--- plugins/fun-diary.js
+++ plugins/fun-diary.js
@@ -193,7 +193,7 @@ cmd({
         await newNote.save();
 
         // Send a formatted message with an image
-        const SUBZERO_DIARY_IMG = 'https://i.ibb.co/cSdrnPMV/mrfrankofc.jpg'; // Replace with your image URL
+        const SUBZERO_DIARY_IMG = 'https://i.postimg.cc/vHQdBJmf/IMG-20250305-WA0006.jpg'; // Replace with your image URL
         const formattedInfo = `📝 *Note added to your diary!✅*\n\n` +
                               `*🔖 "${note}"*\n\n` +
                               `_📆 Time: ${getHarareTime()}_`; // Use Harare time
@@ -250,7 +250,7 @@ cmd({
         await Diary.findByIdAndDelete(noteToDelete._id);
 
         // Send a formatted message with an image
-        const SUBZERO_DIARY_IMG = 'https://i.ibb.co/cSdrnPMV/mrfrankofc.jpg'; // Replace with your image URL
+        const SUBZERO_DIARY_IMG = 'https://i.postimg.cc/vHQdBJmf/IMG-20250305-WA0006.jpg'; // Replace with your image URL
         const formattedInfo = `🗑️ *Note ${noteNumber} deleted from your diary!✅*\n\n` +
                               `*🔖 "${noteToDelete.note}"*\n\n` +
                               `_📆 Time: ${new Date(noteToDelete.timestamp).toLocaleString('en-US', { timeZone: 'Africa/Harare' })}_`; // Use Harare time
@@ -301,7 +301,7 @@ cmd({
         await Diary.deleteMany({ userId: sender });
 
         // Send a formatted message with an image
-        const SUBZERO_DIARY_IMG = 'https://i.ibb.co/cSdrnPMV/mrfrankofc.jpg'; // Replace with your image URL
+        const SUBZERO_DIARY_IMG = 'https://i.postimg.cc/vHQdBJmf/IMG-20250305-WA0006.jpg'; // Replace with your image URL
         const formattedInfo = `💥 *All notes deleted from your diary!✅*\n\n` +
                               `_📆 Time: ${getHarareTime()}_`; // Use Harare time
 
@@ -355,7 +355,7 @@ cmd({
         });
 
         // Send a formatted message with an image
-        const SUBZERO_DIARY_IMG = 'https://i.ibb.co/cSdrnPMV/mrfrankofc.jpg'; // Replace with your image URL
+        const SUBZERO_DIARY_IMG = 'https://i.postimg.cc/vHQdBJmf/IMG-20250305-WA0006.jpg'; // Replace with your image URL
         await conn.sendMessage(from, {
             image: { url: SUBZERO_DIARY_IMG },
             caption: diaryList,
